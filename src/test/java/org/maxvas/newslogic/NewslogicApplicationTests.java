24d0036197f5b7711b78fe5290cdf480049e6e26
Maxim <vasiliev.maxim@gmail.com>
2021-11-17 02:27:48 +0300

    News logic application. Spring initializer. Web, Lombok, Actuator, MongoDb.

---
+++ src/test/java/org/maxvas/newslogic/NewslogicApplicationTests.java
@@ -0,0 +1,13 @@
+package org.maxvas.newslogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewslogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
